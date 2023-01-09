916b316c00ac471efca88434f5539be15a1fd5ef
yogajulianp <yogajprasutiyo@gmail.com>
2023-01-09 08:04:28 +0700

    CRUD 5 controller

--- DotNETMVC/webMVC/Controllers/CustomerController.cs
+++ DotNETMVC/webMVC/Controllers/CustomerController.cs
@@ -18,5 +18,46 @@ namespace webMVC.Controllers
         }
 
         public IActionResult Save([Bind("Id", "Name", "Email", "Password")] CustomerViewModel model)
+        {
+            _customerViewModels.Add(model);
+            return Redirect("List");
+        }
+
+        public IActionResult List()
+        {
+            return View(_customerViewModels);
+        }
+
+        public IActionResult Details(int id)
+        {
+            CustomerViewModel customer = (
+                from p in _customerViewModels
+                where p.Id.Equals(id)
+                select p
+                ).SingleOrDefault(new CustomerViewModel());
+            return View(customer);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            CustomerViewModel customer = _customerViewModels.Find(x => x.Id.Equals(id));
+            return View(customer);
+        }
+
+        public IActionResult Update(int id, [Bind("Id", "Name", "Email", "Password")] CustomerViewModel customer)
+        {
+            CustomerViewModel customerBefore = _customerViewModels.Find(x => x.Id.Equals(id));
+            _customerViewModels.Remove(customerBefore);
+
+            _customerViewModels.Add(customer);
+            return Redirect("List");
+        }
+
+        public IActionResult Delete(int? id)
+        {
+           CustomerViewModel customer = _customerViewModels.Find(x => x.Id.Equals(id));
+            _customerViewModels.Remove(customer);
+            return Redirect("List");
+        }
     }
 }
